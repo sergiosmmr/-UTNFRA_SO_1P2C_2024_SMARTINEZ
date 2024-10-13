9a0382d11ce2d3b4318ade7df30919960cad1d6e
sergiosmmr <sergio_martinez_sen@hotmail.com>
2024-10-13 13:23:20 +0000

    punto E casi completo

--- RTA_SCRIPT_Examen_20241011/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241011/Punto_E.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+DIRECCION="../RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt"
+
+
 #ingreso la direccion donde guardar la info fe memoria
 echo
-echo "la memoria es la siguiente: "
+cat /proc/meminfo | grep 'MemTotal' >> $DIRECCION
 echo
-cat /proc/meminfo | grep 'MemTotal'../RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
+#ingreso la direccion donde guardar la info del chassis
+echo
+sudo dmidecode -t chassis | head -7 | tail -1 >> $DIRECCION
+echo
+echo "se guardo informacion en archivo Filtro_Basico.txt"
 echo
 echo
-
-
+cat $DIRECCION
 
 
