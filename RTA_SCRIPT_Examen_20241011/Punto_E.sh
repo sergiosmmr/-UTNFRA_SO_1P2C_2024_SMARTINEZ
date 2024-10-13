d0be59074898f27828e66c23f071866e70aecba2
sergiosmmr <sergio_martinez_sen@hotmail.com>
2024-10-13 12:56:18 +0000

    cansadisimo

---
+++ RTA_SCRIPT_Examen_20241011/Punto_E.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#ingreso la direccion donde guardar la info fe memoria
+echo
+echo "la memoria es la siguiente: "
+echo
+cat /proc/meminfo | grep 'MemTotal'../RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
+echo
+echo
+
+
+
+
