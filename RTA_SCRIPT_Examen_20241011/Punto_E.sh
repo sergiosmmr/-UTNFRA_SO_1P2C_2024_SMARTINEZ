2f4dbbb2f1193da2957817bfd5f645855e534063
sergiosmmr <sergio_martinez_sen@hotmail.com>
2024-10-13 14:09:06 +0000

    final e

--- RTA_SCRIPT_Examen_20241011/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241011/Punto_E.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-DIRECCION="../RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt"
+DIRECCION="RTA_ARCHIVOS_Examen_20241011/Filtro_Basico.txt"
+
+# creo el archivo en RTA_ARCHIVOS_Examen
+
+touch $DIRECCION
 
 
 #ingreso la direccion donde guardar la info fe memoria
