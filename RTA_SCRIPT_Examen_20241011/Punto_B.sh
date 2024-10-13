c5ce1c54046a18bdf94db51c50a2e28b7ad8c09e
sergiosmmr <sergio_martinez_sen@hotmail.com>
2024-10-13 06:55:08 +0000

    montando

--- RTA_SCRIPT_Examen_20241011/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241011/Punto_B.sh
@@ -71,4 +71,24 @@ sudo mkfs.ext4 ${DISCO}13
 sudo mkfs.ext4 ${DISCO}1
 
 #montaje
+echo "Montando_Particiones......"
 
+INDICE_DISCO=5
+for carpeta in alumno_1/parcial_1 alumno_1/parcial_2 alumno_1/parcial_3 alumno_2/parcial_1 alumno_2/parcial_2 alumno_2/parcial_3 alumno_3/parcial_1 alumno_3/parcial_2 alumno_3/parcial_3 profesores
+do
+	DISPOSITIVO=${DISCO}${INDICE_DISCO}
+	DIR="/Examenes-UTN/${carpeta}"
+
+	echo "Montando disco ${DISCO_A_MONTAR} en ${DIRECTORIO}"
+	echo
+	
+	echo "${DISPOSITIVO}   ${DIR}   ext4     defaults      0        0" | sudo tee -a /etc/fstab 
+	
+	# Aumento contador de disco
+
+	INDICE_DISCO=$((INDICE_DISCO+1))
+done
+
+# Montar todo lo de fstab
+sudo systemctl daemon-reload
+sudo mount -a
